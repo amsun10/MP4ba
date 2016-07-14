1740c868b21e600b86fabf7a3e30e2d6a39d2745
Xiang Zhang <xiang.z.zhang@gmail.com>
2016-07-14 16:49:18 +0800

    initial method structure

---
+++ Constants.py
@@ -0,0 +1,2 @@
+URL = "http://www.mp4ba.com/index.php"
+PAGE_INDEX = "index.php?page=%s"
---
+++ Main.py
@@ -0,0 +1,30 @@
+import requests
+import re
+
+
+
+class CMovieHelper:
+    def __init__(self, url, page_index=0):
+        self.url = url
+        self.page_index = page_index
+        pass
+
+    def get_page_content(self):
+        pass
+
+    def get_page_links(self):
+        pass
+
+    def get_link_torrents(self):
+        pass
+
+    def download_torrents(self, path):
+        pass
+
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass

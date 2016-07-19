e5abc432b95cc1093dc561b6eb2f579381c22ac5
Zhang Xiang <amsun10@gmail.com>
2016-07-19 23:11:02 +0800

    it is the 1st version that could download the torrents successfully and the torrents can be used
    changes:
    1. add file_name and file_extension for download method
    2. optimize download algorithm
    3. remove unused library

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
 import requests
 import re
-import time
 import os
 
 from Constants import *
@@ -21,14 +20,20 @@ class CMovieHelper(object):
         return response.text
 
     def get_download_links(self, content):
-        pat = "\"show.php\?hash=([a-z0-9]*)?\" target=\"_blank\"\>\r\n        ([\s\S]*?)\<\/a\>"
+        pat = "\"(show.php\?hash=[a-z0-9]*)?\" target=\"_blank\"\>\r\n        ([\s\S]*?)\<\/a\>"
         match_object = re.findall(pat, content)
 
         links = []
         if match_object:
-            for hash_code in match_object:
-                download_url = u"%sdown.php?date=%s&hash=%s" % (self.url, int(time.time()), hash_code[0])
-                links.append(download_url)
+            sub_pat = "<a id=\"download\" href=\"([\s\S]*?)\">"
+            for link in match_object:
+                print self.url+link[0]
+                sub_content = requests.get(self.url + link[0]).text
+                try:
+                    download_link = re.findall(sub_pat, sub_content)[0]
+                    links.append((self.url + download_link, link[1]))
+                except IndexError:
+                    raise
 
         return links
 
@@ -47,37 +52,48 @@ class CMovieHelper(object):
                     raise
         return links
 
-    def download_torrents(self, link, dir_path=None):
+    def download_torrents(self, link, file_name=None, dir_path=None):
         if not dir_path:
             dir_path = os.path.join(os.getcwd(), "torrents")
 
         if not os.path.isdir(dir_path):
             os.makedirs(dir_path)
 
-        with DownloadRequest(url=link, path=dir_path) as downloadHelper:
+        with DownloadRequest(url=link, path=dir_path, file_name=file_name, ext="torrent") as downloadHelper:
             path = downloadHelper.download()
             return True, path
-
 
     def run(self):
         content = self.get_page_content()
         # print content
         links = self.get_download_links(content)
         print links
+        # return
         for link in links:
-            self.download_torrents(link)
+            self.download_torrents(link[0], file_name=link[1])
         pass
 
+
 class DownloadRequest(object):
-    def __init__(self, session=requests, url="", path="\\"):
+    def __init__(self, session=requests, url="", path="\\", file_name=None, ext=None):
         self.session = session
         self.url = url
         self.path = path
+        self.ext = ext
+        self.file_name = file_name
         self.response = None
 
     def download(self):
-        print "downlaoding: %s" % self.url
-        local_filename = self.url.split('/')[-1]
+        print "downloading: %s" % self.url
+
+        if self.file_name:
+            local_filename = self.file_name
+        else:
+            local_filename = self.url.split('/')[-1]
+
+        if self.ext:
+            local_filename += ".%s" % self.ext
+
         local_file_path = os.path.join(self.path, local_filename)
         r = requests.session().get(self.url)
         with open(local_file_path, 'wb') as f:
@@ -85,7 +101,6 @@ class DownloadRequest(object):
                 if chunk:  # filter out keep-alive new chunks
                     f.write(chunk)
         return os.path.abspath(local_file_path)
-
 
     def __enter__(self):
         return self

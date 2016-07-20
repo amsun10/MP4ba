3e31bf22e7ff5b55c28657f20b370486f2287cbc
xiang.zhang <xiang.z.zhang@gmail.com>
2016-07-20 11:00:26 +0800

    1. update demo code

--- demo.py
+++ demo.py
@@ -3,8 +3,9 @@ from Constants import *
 
 if __name__ == '__main__':
 
-    myMovieHelper = CMovieHelper(URL)
+    myMovieHelper = CMovieHelper(URL, dir_path="D:\\mp4ba_torrents")
     # set which page you want to download
-    myMovieHelper.set_page_index(1)
-    myMovieHelper.run()
+    for i in range(1, 125):
+        myMovieHelper.set_page_index(i)
+        myMovieHelper.run()
     pass

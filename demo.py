e376e91371053622ae7501b5aa64839335e1c7ad
xiang.zhang <xiang.z.zhang@gmail.com>
2016-07-29 22:03:19 +0800

    1. update demo code 2. add a bat for windows which used to download recent 5 days torrents

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 
     myMovieHelper = CMovieHelper(URL, dir_path="D:\\mp4ba_torrents")
     # set which page you want to download
-    for i in range(1, 125):
+    for i in range(1, 5):
         myMovieHelper.set_page_index(i)
         myMovieHelper.run()
     pass
